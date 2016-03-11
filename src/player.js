cead79a750078d085a40e753412330dcd891c4f1
nekobato <nekobato@gmail.com>
2016-03-11 20:44:30 +0900

    fixed player component timing

--- src/player.js
+++ src/player.js
@@ -18,18 +18,20 @@ window.app = new Vue({
   events: {
     'filer:select-file': function(fileStr) {
       let file = JSON.parse(fileStr);
-      this.playMode = 'video-player';
-      this.$broadcast('player:receive-file', file);
+      this.viewMode = 'video-player';
+      this.$nextTick(function() {
+        this.$broadcast('player:receive-file', file);
+      });
     },
     'url:submit-url': function(url) {
-      console.log(url);
-      this.playMode = 'webview';
-      this.$broadcast('player:receive-url', url);
+      this.viewMode = 'webview';
+      this.$nextTick(function() {
+        this.$broadcast('player:receive-url', url);
+      });
     }
   },
   ready: function() {
     ipcRenderer.on('main:ipc-bridge', (event, channel, data) => {
-      console.log(channel, data);
       this.$emit(channel, data);
     });
   }
