b11bf4e2a42256bc938fcad9ee6600370bbe9adc
koriyama <nekobato@gmail.com>
2016-03-11 18:16:49 +0900

    fixed icon file path

--- app/main.js
+++ app/main.js
@@ -19,7 +19,7 @@ app.on('ready', function() {
   player = new PlayerWindow();
   controller = new ControllerWindow();
 
-  trayIcon = nativeImage.createFromPath(__dirname + '/tray_icon.png');
+  trayIcon = nativeImage.createFromPath(__dirname + '/img/tray_icon.png');
   tray = new Tray(trayIcon);
 
   tray.on('click', function(event, bounds) {
