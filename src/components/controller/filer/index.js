8f7aa1b1b7f1f9833e8628a4ba20dc3c5176f088
nekobato <nekobato@gmail.com>
2016-05-29 00:13:11 +0900

    remember filenev depth

--- src/components/controller/filer/index.js
+++ src/components/controller/filer/index.js
@@ -1,9 +1,9 @@
-const electron = require('electron');
-const ipcRenderer = electron.ipcRenderer;
-const remote      = electron.remote;
-const fs = remote.require('fs');
-const path = remote.require('path');
-import style from './style.styl';
+const electron = require('electron')
+const ipcRenderer = electron.ipcRenderer
+const remote      = electron.remote
+const fs = remote.require('fs')
+const path = remote.require('path')
+import style from './style.styl'
 
 export default {
   template: require('./template.jade')(),
@@ -20,9 +20,9 @@ export default {
   filters: {
     file2IconName: function(file) {
       if (/\.(mp4|mpe?g)$/.test(file.name)) {
-        return "video_library";
+        return "video_library"
       } if (file.type === "directory") {
-        return "folder";
+        return "folder"
       }
     }
   },
@@ -34,23 +34,23 @@ export default {
   methods: {
     selectItem: function(file) {
       if (file.type === 'directory') {
-        this.$emit('getDir', file);
+        this.$emit('getDir', file)
       } else if (file.type === 'file') {
-        ipcRenderer.send('controller:ipc-bridge', 'filer:select-file', JSON.stringify(file));
+        ipcRenderer.send('controller:ipc-bridge', 'filer:select-file', JSON.stringify(file))
       }
     },
     onSelectDepth: function(file, depth) {
-      this.$data.depth.length = depth;
-      this.$emit('getDir', file);
+      this.$data.depth.length = depth
+      this.$emit('getDir', file)
     },
     getDir: function(file) {
-      this.$data.reaction.loadingDir = true;
+      this.$data.reaction.loadingDir = true
 
       if (fs.statSync(file.path).isDirectory()) {
-        let finder = [];
+        let finder = []
         for(var p of fs.readdirSync(file.path)) {
-          if (/^\..*/.test(p)) { continue; }
-          let stats = fs.statSync(path.join(file.path, p));
+          if (/^\..*/.test(p)) { continue }
+          let stats = fs.statSync(path.join(file.path, p))
           finder.push({
             name: p,
             path: path.join(file.path, p),
@@ -58,22 +58,31 @@ export default {
           })
         }
 
-        this.$emit('filer-set-dir', finder);
-        this.addDepth(file);
-        this.$data.reaction.loadingDir = false;
+        this.$emit('filer-set-dir', finder)
+        this.addDepth(file)
+        this.$data.reaction.loadingDir = false
       }
     },
     setDir: function(files) {
-      this.$set('filelist', files);
+      this.$set('filelist', files)
+      window.localStorage.setItem('depth', JSON.stringify(this.$data.depth))
     },
     addDepth: function(file) {
-      this.$data.depth.push(file);
+      this.$data.depth.push(file)
     },
     addFilesAll: function() {
-      this.$dispatch('all', 'files:get', this.$data.filelist);
+      this.$dispatch('all', 'files:get', this.$data.filelist)
     }
   },
   ready: function() {
-    this.getDir({ path: '/Users', name: '/' });
+    const depth = JSON.parse(window.localStorage.depth) || []
+    // Resurrect
+    if (depth.length) {
+        const currentDir = depth[depth.length-1]
+        this.$data.depth = depth
+        this.getDir(currentDir)
+    } else {
+      this.getDir({ path: '/', name: '/' })
+    }
   }
 }
