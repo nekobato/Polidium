16e89e741b72aefb479b13b9fff99a4372fe026c
nekobato <nekobato@gmail.com>
2017-03-01 23:54:47 +0900

    disable pointer-events when click-through

--- app/src/renderer/Player/Player.vue
+++ app/src/renderer/Player/Player.vue
@@ -22,7 +22,8 @@ module.exports = {
     },
     playerStyle () {
       return {
-        opacity: this.settings.resizeMode ? 1 : this.settings.opacity
+        opacity: this.settings.resizeMode ? 1 : this.settings.opacity,
+        'pointer-events': this.settings.clickThrough ? 'none' : 'auto'
       }
     }
   }
