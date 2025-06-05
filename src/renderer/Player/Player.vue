440fa16c70da665af20523f5218a557cd8ef2983
Hayato Koriyama <nekobato@gmail.com>
2025-06-05 16:31:19 +0900

    Use router for player switching

--- src/renderer/Player/Player.vue
+++ src/renderer/Player/Player.vue
@@ -1,22 +1,33 @@
 <template>
   <div class="player" :style="playerStyle">
-    <component :is="settings.mode" />
+    <router-view />
     <resize-mode v-show="settings.resizeMode" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, watch } from "vue";
 import type { CSSProperties } from "vue";
+import { useRouter } from "vue-router";
 import { useSettingsStore } from "@/renderer/store/modules/settings";
 import ResizeMode from "./ResizeMode.vue";
 
 const settingsStore = useSettingsStore();
 const settings = computed(() => settingsStore.player);
+const router = useRouter();
 const playerStyle = computed<CSSProperties>(() => ({
   opacity: settings.value.resizeMode ? 1 : settings.value.opacity,
   pointerEvents: settings.value.clickThrough ? "none" : "auto"
 }));
+
+watch(
+  () => settings.value.mode,
+  (mode) => {
+    if (mode === "video-player") router.push("/player/file");
+    else if (mode === "web-player") router.push("/player/web");
+  },
+  { immediate: true }
+);
 </script>
 
 <style lang="scss">
