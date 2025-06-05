7fa99f73bc226aae43742a47a7439a49f5c77ef0
nekobato <nekobato@gmail.com>
2025-06-05 11:35:23 +0900

    remove test

--- app/src/renderer/Player/index.ts
+++ app/src/renderer/Player/index.ts
@@ -1,8 +1,7 @@
-import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import store from '../store'
-import Player from './Player.vue'
-import 'material-design-icons/iconfont/material-icons.css'
+import { createApp } from "vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import store from "../store";
+import Player from "./Player.vue";
 
-createApp(Player).use(store).use(ElementPlus).mount('#player')
+createApp(Player).use(store).use(ElementPlus).mount("#player");
